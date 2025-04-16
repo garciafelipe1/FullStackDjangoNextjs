6f7508b81a8fea6c84771de36ff96235bdc57143
Felipe <feli2003garcia@gmail.com>
2025-04-15 23:39:17 -0300

    modificated back

--- backend/apps/user_profile/urls.py
+++ backend/apps/user_profile/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from .views import MyUserProfileView,UpdateUserProfileView
+from .views import MyUserProfileView,UpdateUserProfileView,GetMyProfilePictureView,UploadProfilePictureView
 
 urlpatterns = [
     path("my_profile/", MyUserProfileView.as_view(), name="my-profile-view"),
-    path("update/", UpdateUserProfileView.as_view(), name="update"),
+    path("update/", UpdateUserProfileView.as_view()),
+    path("get_picture/", GetMyProfilePictureView.as_view()),
+    path("upload_picture/", UploadProfilePictureView.as_view()),
+    
+    
     
 ]
--- backend/apps/user_profile/views.py
+++ backend/apps/user_profile/views.py
@@ -1,15 +1,21 @@
-from rest_framework import permissions
-from rest_framework_api.views import StandardAPIView
+
+from rest_framework import permissions,status   
+from rest_framework_api.views import StandardAPIView,APIView
 from django.contrib.auth import get_user_model
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from datetime import datetime, timedelta
 from .models import UserProfile
 from .serializers import UserProfileSerializer
 from django.core.exceptions import ValidationError
+from rest_framework.response import Response
 User = get_user_model()
+from django.http import FileResponse, Http404
 from utils.string_utils import sanitize_string,sanitize_html,sanitize_username,sanitize_url
+from rest_framework.parsers import MultiPartParser, FormParser
+import os
 
-User = get_user_model()
+
+from django.shortcuts import get_object_or_404
 
 class MyUserProfileView(StandardAPIView):
     permission_classes = [ permissions.IsAuthenticated]
@@ -20,6 +26,23 @@ class MyUserProfileView(StandardAPIView):
         user_profile = UserProfile.objects.get(user=user)
         serialized_user_profile = UserProfileSerializer(user_profile).data
         return self.response(serialized_user_profile)
+    
+class GetMyProfilePictureView(StandardAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+    authentication_classes = [JWTAuthentication]
+
+    def get(self, request):
+        user = request.user
+        profile = get_object_or_404(UserProfile, user=user)
+        print(f"Profile picture: {profile.profile_picture}")
+        if not profile.profile_picture:
+            return Response("No profile picture found.", status=404)
+
+        picture_url = request.build_absolute_uri(profile.profile_picture.url) 
+
+        return self.response({"profile_picture_url": picture_url})
+    
+    
 class UpdateUserProfileView(StandardAPIView):
     permission_classes = [permissions.IsAuthenticated]
     authentication_classes = [JWTAuthentication]
@@ -70,3 +93,23 @@ class UpdateUserProfileView(StandardAPIView):
             return self.response("Profile has been updated successfully.")
         except ValidationError as e:
             return self.error(str(e))
+
+
+
+class UploadProfilePictureView(StandardAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+    authentication_classes = [JWTAuthentication]
+    parser_classes = [MultiPartParser, FormParser]
+
+    def post(self, request):
+        user = request.user
+        profile = UserProfile.objects.get(user=user)
+
+        uploaded_file = request.FILES.get("file")
+        if not uploaded_file:
+            return self.response("No file uploaded.", status=400)
+
+        profile.profile_picture = uploaded_file  # Django se encarga de guardarlo
+        profile.save()
+
+        return self.response("Profile picture has been updated.")
